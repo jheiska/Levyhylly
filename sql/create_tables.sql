c1e931eed79268a3ea11a348ddf017e25679b9ca
Heiska Jaakko T K <hcjaakko@da5-cs-b221-29.pc.helsinki.fi>
2017-03-30 16:29:34 +0300

    create

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,27 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Collector(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    password VARCHAR NOT NULL
+);
+
+CREATE TABLE Record(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    artist VARCHAR,
+    year INTEGER
+);
+
+CREATE TABLE Track(
+    id SERIAL PRIMARY KEY,
+    record_id INTEGER REFERENCES Record(id),
+    name VARCHAR NOT NULL,
+    runningtime VARCHAR
+);
+
+CREATE TABLE Person(
+    id SERIAL PRIMARY KEY,
+    record_id INTEGER REFERENCES Record(id),
+    first_name VARCHAR,
+    last_name VARCHAR,
+    nick_name VARCHAR  
+);
