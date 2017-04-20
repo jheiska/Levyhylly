9830b1a2e5935d999996e4166d58ccee9c9cf0e4
jheiska <jaakkoheiska@gmail.com>
2017-04-20 15:58:25 +0300

    säätöä

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -7,23 +7,23 @@ class HelloWorldController extends BaseController {
         echo 'Tämä on etusivu!';
     }
 
-    public static function sandbox() {
-        $nightfall = record::find(1);
-        $records = record::all();
-        // Kint-luokan dump-metodi tulostaa muuttujan arvon
-        Kint::dump($records);
-        Kint::dump($nightfall);
-    }
-
 //    public static function sandbox() {
-//        $rec = new record(array(
-//            'name' => '',
-//            'artist' => 'BG',
-//            'year' => '2018'            
-//        ));
-//        $errors = $rec->errors();
-//
-//        Kint::dump($errors);
+//        $nightfall = record::find(1);
+//        $records = record::all();
+//        // Kint-luokan dump-metodi tulostaa muuttujan arvon
+//        Kint::dump($records);
+//        Kint::dump($nightfall);
 //    }
+
+    public static function sandbox() {
+        $rec = new record(array(
+            'name' => '',
+            'artist' => 'BG',
+            'year' => '2018'            
+        ));
+        $errors = $rec->errors();
+
+        Kint::dump($errors);
+    }
 
 }
